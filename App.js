a8b1ba450a7e91c5ae234191a715fa9118647ce0
ryunaeun <naeun9822434@gmail.com>
2024-09-20 19:27:42 +0900

    Init: FE initial commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>knu bus app</Text>
+      <Text>knu bus</Text>
       <StatusBar style="auto" />
     </View>
   );
